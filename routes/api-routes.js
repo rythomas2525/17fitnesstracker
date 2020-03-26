5f2162bf2f450837c824e2c1df577afdc9b15bba
Ryan Thomas <rythomas2525@gmail.com>
2020-03-26 11:07:54 -0600

    api routes started and mvc done

---
+++ routes/api-routes.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const workout = require("../models/workout");
+
+router.post("/api/workout", ({ body }, res) => {
+    workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workout", ({ body }, res) => {
+    workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workout/range", (req, res) => {
+    workout.find({})
+        .sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
